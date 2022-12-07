80078aea8e25a7cecc45d57c06a2e633a44bc21c
erica <erica.du0916@gmail.com>
2022-12-07 19:30:12 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:ericacadu/***.git master:gh-pages
+git push -f git@github.com:ericacadu/20221101.git master:gh-pages
 cd -
